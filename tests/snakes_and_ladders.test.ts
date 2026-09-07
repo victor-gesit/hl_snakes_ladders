c86d2df991b24d95363e8c0ee55cfe39eaa9d550
Victor Idongesit <victoridongesit@gmail.com>
2026-09-07 15:37:19 +0100

    add passing tests

---
+++ tests/snakes_and_ladders.test.ts
@@ -0,0 +1,13 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('As a player I want to be able to move my token So that I can get closer to the goal', () => {
+    test('Game starts with token on square 1', () => {
+        expect(true).toBe(true)
+    });
+});
+
+describe('As a player I want to move my token based on the roll of a die So that there is an element of chance in the game', () => {
+    test('Dice roll is between 1 and 6, inclusive', () => {
+        expect(true).toBe(true)
+    });
+});
